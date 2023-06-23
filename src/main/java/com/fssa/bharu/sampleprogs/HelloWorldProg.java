6bda2bf94e2cee81407b04e6538b81f125add129
Santhanam <116251909+santhanam13@users.noreply.github.com>
2023-06-23 10:22:19 +0530

    Adding my maven proj into gitup

---
+++ src/main/java/com/fssa/bharu/sampleprogs/HelloWorldProg.java
@@ -0,0 +1,10 @@
+package com.fssa.bharu.sampleprogs;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Hello World");
+	}
+
+}
